88594cdba673df74c381d9982dfa10a790929687
Kim Leung <kleung@contently.com>
2017-08-24 01:30:59 -0400

    just got streams to work and retrieving the latest stream

--- src/index.js
+++ src/index.js
@@ -1,32 +1,80 @@
 const Alexa = require("alexa-sdk");
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1"});
+const dynamodbstreams = new AWS.DynamoDBStreams({apiVersion: "2012-08-10"});
+const Speech = require("ssml-builder");
 
-var topics;
-var handlers = {
-  'LaunchRequest': function () {
-    this.emit(':tell', 'Hello World');
+let topics;
+let handlers = {
+  "LaunchRequest": function() {
+    let speech = new Speech();
+    speech.say("I am the Vision, aka Paul Bettany.")
+    speech.pause("500ms")
+    speech.say("What would you like to know?")
+    let speechOutput = speech.ssml(true);
+    this.emit(":ask", speechOutput)
   },
-  'TopicIntent': function () {
-    this.emit(':tell', 'Test Photo topics: ' + topics);
+  "TopicIntent": function () {
+    this.emit(":tell", "Your last photo contained, " + topics);
+  },
+  "AMAZON.HelpIntent": function() {
+    this.emit(":tell", "Ask me what is in your last photo!");
   }
 }
 
 exports.handler = (event, context, callback) => {
-  getTopics(callback, function() {
-    alexaFunction(event, context);
-  });
+  // getTopics(callback, function() {
+  //   alexaFunction(event, context);
+  // });
+  getLatestFromPhoto(callback);
 };
 
-function alexaFunction(event, context) {
-  var alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+function getLatestFromPhoto(lambdaCallback) {
+  let streamArn = "arn:aws:dynamodb:us-east-1:351331127751:table/photos/stream/2017-08-24T04:37:16.067"
+  let params = {
+    StreamArn: streamArn
+  };
+
+  dynamodbstreams.describeStream(params).promise()
+    .then(function(data) {
+      console.log("## of shards ## " + data["StreamDescription"]["Shards"].length)
+      let shardId = data["StreamDescription"]["Shards"][0]["ShardId"];
+      return shardId
+    }).then(function(shardId) {
+      params = {
+        ShardId: shardId,
+        ShardIteratorType: "LATEST",
+        StreamArn: streamArn
+      }
+
+      return dynamodbstreams.getShardIterator(params).promise()
+        .then(function(data) {
+          let shardIterator = data["ShardIterator"]
+          return shardIterator
+        }).catch(function(err) {
+          lambdaCallback(err, null)
+        });
+    }).then(function(shardIterator) {
+      params = {
+        ShardIterator: shardIterator,
+        Limit: 1
+      }
+
+      dynamodbstreams.getRecords(params, function(err, data) {
+        if (err) {
+          lambdaCallback(err, null)
+        } else {
+          lambdaCallback(null, data)
+        };
+      });
+    }).catch(function(err) {
+      console.log("Stream error: " + err);
+    });
 };
 
 function getTopics(callback, alexaCallback) {
   setTimeout (function() {
-    var params = {
+    let params = {
       TableName: "photos",
       Key: {
         photoId: 2
@@ -45,4 +93,10 @@ function getTopics(callback, alexaCallback) {
     alexaCallback()
 
   }, 1000)
+};
+
+function alexaFunction(event, context) {
+  let alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
